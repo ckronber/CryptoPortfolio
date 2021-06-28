efa1130faf12445d87b38e6f14078bf06eafb8f4
Chris Kronberg <ckronber@gmail.com>
2021-06-28 10:28:29 -0400

    Add project files.

---
+++ CryptoPortfolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CryptoPortfolio.Startup))]
+namespace CryptoPortfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
