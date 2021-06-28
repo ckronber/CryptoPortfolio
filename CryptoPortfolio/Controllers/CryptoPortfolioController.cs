6f763c30c2f5e0b5ea2d3b56e10a78c6a7609a93
Chris Kronberg <ckronber@gmail.com>
2021-06-28 11:44:52 -0400

    Files and folders added for services and controllers

---
+++ CryptoPortfolio/Controllers/CryptoPortfolioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CryptoPortfolio.Controllers
+{
+    public class CryptoPortfolioController : Controller
+    {
+        // GET: CryptoPortfolio
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CryptoPortfolio/Controllers/CryptoPurchaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CryptoPortfolio.Controllers
+{
+    public class CryptoPurchaseController : Controller
+    {
+        // GET: CryptoPurchase
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
