ceb976fbe7bb15e6c054cae0e6c24d5b61ef7a69
Chris Kronberg <ckronber@gmail.com>
2021-06-28 11:26:47 -0400

    Added References and Nuget Packages Updated

--- Data/CryptoInfo.cs
+++ Data/CryptoInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace Data
 {
     public class CryptoInfo
     {
+        [Key]
+        public int CryptoId { get; set; }
+        [Required]
+        public string Currency { get; set; }
+        [Required]
+        public string CryptoName { get; set; }
+        public virtual decimal Price { get; set; }
+        public decimal Amount { get; set; }
+        public decimal TotalValue { get; set; }
+
+        // To be added later. This is a stretch goal
+        //public int ExchangeId { get; set; }
+        //public Exchange Exchange { get; set; }
+
     }
 }
