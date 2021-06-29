dbd82b81fb550f80feb3842918e5c258eabf4f34
Chris Kronberg <ckronber@gmail.com>
2021-06-28 23:30:44 -0400

    Bootstrap version updated

--- CryptoPortfolio/Controllers/AccountController.cs
+++ CryptoPortfolio/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using CryptoPortfolio.Models;
+using CryptoPortfolio.Data;
 
 namespace CryptoPortfolio.Controllers
 {
